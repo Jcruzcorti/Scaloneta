a298d0651d65dd3deb28ef0a98220570578ab520
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-10-30 17:00:26 -0300

    Título nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </header>
 
         <section className="App-section">       
-          <ItemListContainer greet="FINALISSIMA POR UN LOMITO PA: 10 PREGUNTAS - 100 SEGUNDOS"/>        
+          <ItemListContainer greet="FINALISSIMA POR UN LOMITO PA"/>        
         </section>
     </div>
   );
